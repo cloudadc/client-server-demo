7a9f835d8fcf7f208e5e4621d15eaaf38b606501
kylin <kylinsoong.1214@gmail.com>
2020-07-29 00:25:06 +0800

    finish frontend

--- frontend/src/main/java/com/example/servingwebcontent/FrontendController.java
+++ frontend/src/main/java/com/example/servingwebcontent/FrontendController.java
@@ -1,14 +1,63 @@
 package com.example.servingwebcontent;
 
-import org.springframework.stereotype.Controller;
-import org.springframework.web.bind.annotation.GetMapping;
+import java.util.ArrayList;
+import java.util.List;
 
-@Controller
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.boot.web.client.RestTemplateBuilder;
+import org.springframework.context.annotation.Bean;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RequestMethod;
+import org.springframework.web.bind.annotation.RestController;
+import org.springframework.web.client.RestTemplate;
+
+@RestController
+@RequestMapping(produces = "text/plain;charset=UTF-8")
 public class FrontendController {
+	
+	@Autowired
+	private RestTemplate restTemplate;
 
-	@GetMapping("/getAll")
+	@SuppressWarnings("unchecked")
+	@RequestMapping(path = {"/ui"}, method = {RequestMethod.GET})
 	public String getAll() {
-		return "greeting";
+		
+		String host = env("BACKEND_HOST");
+				
+		String port = env("BACKEND_PORT");
+		
+		//System.out.println(host + " " + port );
+		
+		List<Object> fruits = restTemplate.getForObject("http://" + host + ":" + port + "/api/fruits", ArrayList.class);
+		
+		StringBuffer sb = new StringBuffer();
+		fruits.forEach(f -> {
+			sb.append(f).append("\n");
+		});
+		
+		return sb.toString();
 	}
+	
+	private String env(String key) {
+		
+		String value = System.getProperty(key);
+		
+		if(null != value) {
+			return value;
+		}
+		
+		value = System.getenv().get(key);
+		
+		if(null != value) {
+			return value;
+		}
+		
+		throw new RuntimeException("Can not get the value of " + key);
+	}
+
+	@Bean
+    public RestTemplate restTemplate(RestTemplateBuilder builder) {
+            return builder.build();
+    }
 
 }
