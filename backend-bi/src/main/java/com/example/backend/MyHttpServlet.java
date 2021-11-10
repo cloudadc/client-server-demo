0ec2337d49260ae9d731033b9f6fefe014959c1a
kylin <kylinsoong.1214@gmail.com>
2021-11-10 23:18:50 +0800

    add bi rpt backend

---
+++ backend-bi/src/main/java/com/example/backend/MyHttpServlet.java
@@ -0,0 +1,29 @@
+package com.example.backend;
+
+import java.io.IOException;
+
+import javax.servlet.ServletException;
+import javax.servlet.annotation.WebServlet;
+import javax.servlet.http.HttpServlet;
+import javax.servlet.http.HttpServletRequest;
+import javax.servlet.http.HttpServletResponse;
+
+@WebServlet(urlPatterns = "/webroot/decision", loadOnStartup = 1)
+public class MyHttpServlet extends HttpServlet {
+
+	private static final long serialVersionUID = 8519374420859509559L;
+
+	@Override
+	protected void doGet(HttpServletRequest req, HttpServletResponse resp) throws ServletException, IOException {
+//		resp.setContentType("text/html");
+//		PrintWriter out = resp.getWriter();
+//	     out.println("<h3>Hello India!</h3>");
+		resp.sendRedirect("/webroot/decision/login");
+	}
+
+	@Override
+	protected void doPost(HttpServletRequest req, HttpServletResponse resp) throws ServletException, IOException {
+		doGet(req, resp);
+	}
+
+}
---
+++ backend-rpt/src/main/java/com/example/backend/foo/WebController.java
@@ -0,0 +1,17 @@
+package com.example.backend.foo;
+
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.ResponseBody;
+import org.springframework.web.bind.annotation.RestController;
+
+@RestController
+public class WebController {
+
+	
+	
+	@RequestMapping("/webroot/decision/login")
+    @ResponseBody
+    public String login(){
+        return "<h1>RPT Login Page.. !</h1>";
+    }
+}
